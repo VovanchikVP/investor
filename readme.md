1e6a7f1757723558271901828d513911a6b53a66
Владимир Капитонов <vladimirkapitonov@MacBook-Pro-Vladimir.local>
2023-05-23 12:54:18 +1000

    test

--- readme.md
+++ readme.md
@@ -5,14 +5,7 @@
 3. Запуск сервиса `uvicorn src.main:app`
 
 ### API:
-1. Получение отчета: `POST /api/v1/reports` (json файл для теста .temp/test_data.json)
-
-### Запрос
-`curl -X 'POST' \
-  'http://127.0.0.1:8000/api/v1/reports' \
-  -H 'accept: application/json' \
-  -H 'Content-Type: application/json' \
-  -d '@temp/test_data.json'`
+1. Доступ к swagger: `/docs`
 
 ### Тесты
 Запуск тестов `pytest`
--- src/tests/test_services.py
+++ src/tests/test_services.py
@@ -1,24 +1,6 @@
 import asyncio
-import json
-import os
-import pathlib
 
 from fastapi.responses import FileResponse
-
-from src.app.api.schemes import (
-    BaseReportSchema,
-    JsReportsDeclarationSchema,
-)
-from src.app.services.transformation_data import Transformation
-
-
-def get_data_for_tests() -> Transformation:
-    path = os.path.join(pathlib.Path(__file__).absolute().parents[2], pathlib.Path("temp/test_data.json"))
-    with open(path) as f:
-        json_data = json.load(f)
-    obj = BaseReportSchema(**json_data)
-    host = "localhost"
-    return Transformation(host, obj)
 
 
 def test_get_reports():
